9aff31fa117b1ecf8fe126e29f69d4d782f5b0c6
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-22 10:44:00 +0530

    [vijayalakshmi] established database connection

---
+++ ADO_EmployeePayRoll/EmployeePayroll/PayrollSystem.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    internal class PayrollSystem
+    {
+        public static string dbpath = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PayRoll;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(dbpath);
+        //Fuction to check DB connection Establishment
+        public void DatabseConnection()
+        {
+            try
+            {
+                connect.Open();
+                using (connect)
+                {
+                    Console.WriteLine("Database connectivity successful.");
+                }
+                connect.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Database connectivity failed");
+            }
+        }
+
+    }
+}
---
+++ ADO_EmployeePayRoll/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+EmployeePayroll.PayrollSystem payrollSystem = new EmployeePayroll.PayrollSystem();
+payrollSystem.DatabseConnection();
