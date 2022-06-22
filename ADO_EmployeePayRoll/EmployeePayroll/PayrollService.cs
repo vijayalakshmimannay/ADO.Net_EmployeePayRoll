008922787f49ae921224d1937a88f83b5ff409bf
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-22 10:54:03 +0530

    [vijayalakshmi] established database connection

--- ADO_EmployeePayRoll/EmployeePayroll/PayrollService.cs
+++ ADO_EmployeePayRoll/EmployeePayroll/PayrollService.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -8,12 +9,12 @@ using System.Threading.Tasks;
 
 namespace EmployeePayroll
 {
-    internal class PayrollSystem
+    internal class PayrollService
     {
         public static string dbpath = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PayRoll;Integrated Security=True";
         SqlConnection connect = new SqlConnection(dbpath);
         //Fuction to check DB connection Establishment
-        public void DatabseConnection()
+        public void DatabaseConnection()
         {
             try
             {
